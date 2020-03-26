8b2aa637b01d7e3e72f9b0ce126353e86f7105ea
ManvikaG <62579982+ManvikaG@users.noreply.github.com>
2020-03-26 11:34:08 -0700

    Update falldetection.py

--- falldetection.py
+++ falldetection.py
@@ -1,16 +1,8 @@
 from adafruit_circuitplayground import cp
 
-if cp.shake(100):
-    print("Hard Shake Detected!")
-    cp.pixels[9] = (255, 0, 0)
-    cp.pixels[8] = (255, 0, 0)
-    cp.pixels[7] = (255, 0, 0)
-    cp.pixels[6] = (255, 0, 0)
-    cp.pixels[5] = (255, 0, 0)
-    cp.pixels[4] = (255, 0, 0)
-    cp.pixels[3] = (255, 0, 0)
-    cp.pixels[2] = (255, 0, 0)
-    cp.pixels[1] = (255, 0, 0)
-    cp.pixels[0] = (255, 0, 0)
-    cp.play_tone(440, 10.0)
+while True:
+    if cp.shake(50):
+        print("Hard shake Detected")
+        cp.pixels.fill((255, 0, 0))
+        cp.play_tone(440, 10.0)
 
